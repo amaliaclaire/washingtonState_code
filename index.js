89e7ac610c24aef454c172b79ad9f0bd4d617ea1
Amalia <amalia.fergin@gmail.com>
2019-04-25 00:23:01 -0700

    mewo

---
+++ index.js
@@ -0,0 +1,106 @@
+let dataArray = require('./data.js');
+let namesOfStates = require('./usaStates.js')
+
+// data normalization
+// console.log(dataArray);
+
+
+
+
+
+// uniqueArray(dataArray)
+
+
+// an array of objects with a location of a key value pair
+//  users insert the state that they are residing in
+// need to convert all state names (example: washington) to WA.
+// ensure end result will be .uppercase()
+// array of words that trigger that function "washington, state of washington, washginton state" to format to WA
+//  if object of array has "washington or WA in string, separate and push it into a string"
+// convert it where it can be "held" and then
+
+// if it contains washington or wa
+// accurate -- pump the addresses into google maps api, give back a formated address provience or street address. It might cost money, free amount -- so many api requests for free, don't know the actual scale.
+//  .sort()
+
+
+// idea --- filter the array of strings and push all the strings that only have "WA".
+//  all the left over ones, you will have to do the if else statement
+// insert it back into the table
+// name them state abbrivations
+// console.log('dataOfStates', namesOfStates);
+//Babies first go around probably just looks for the two letter identifier or the “substring” of the full state name
+// .push element into a variable or hold it somewhere.
+// look at libraries that give you close matches
+//https://stackoverflow.com/questions/10018679/python-find-closest-string-from-a-list-to-another-string#10018734
+
+
+let washingtonArray = namesOfStates.map(a => a.location)
+let element = ''
+
+
+
+
+  let test = ((array) => {
+    let result
+    let newArray = []
+
+
+      for (let i = 0; i < array.length; i++) {
+        if(array[i] != 'WA' ) {
+          console.log(array[i]);
+
+          
+          // array[i] is the rest of the strings other than "WA" and you are assigning them all to become "WA"
+            // console.log(newArray.push(array[i]))
+
+             // newArray
+
+            // have all the values be overrided and return 'WA'
+        }
+         newArray
+      }
+
+  })
+
+  test(washingtonArray)
+
+
+  // for loop [i]
+  // if it is NOT "WA" then proceed below. If it is WA then just return the string.
+  // iterate over each one and see if they are already "assigned" by the "WA" substring of the full state name
+  // have the end result be all WA
+
+
+
+
+  //  washingtonArray.forEach((e) => {
+  //    element = e
+  //    console.log('element', element);
+  //
+  //    return element
+  // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lll
---
+++ usaStates.js
@@ -0,0 +1,29 @@
+const namesOfStates =
+[
+  {
+    location: "washington",
+  },
+  {
+    location: "state of washington",
+  },
+  {
+    location: "WA state",
+  },
+  {
+    location: "WA",
+  },
+  {
+    location: "washington",
+  },
+  {
+    location: "WA",
+  },
+  {
+    location: "washington state",
+  },
+  {
+    location: "Washington",
+  }
+]
+
+module.exports = namesOfStates;
